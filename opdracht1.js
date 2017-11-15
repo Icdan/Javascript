3c02aa590c70bdf72889c83699b52e7faa958b48
Icdan <li215@live.nl>
2017-11-15 14:34:35 +0100

    Opdracht 1

---
+++ opdracht1.js
@@ -0,0 +1,14 @@
+console.log("Hello world!")
+
+for(i = 0; i < 10; i++){
+    console.log("De waarde van i is " + i);
+}
+
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+console.log("De kleur van mijn auto is " + mijnauto.kleur);
