a9403880afb4ebed3ac7d979f8c5d5335760e05a
Icdan <li215@live.nl>
2017-12-08 10:29:00 +0100

    Images, update 5

--- opdracht5.js
+++ opdracht5.js
@@ -1,83 +1,42 @@
-addImage();
+pics = document.getElementById("pics");
+createPicsHolders();
+createSaiyanPics();
 
-function addImage() {
+function createPicsHolders() {
     for (var i = 0; i < 9; i++) {
-        saiyanPic = document.getElementsByClassName("classimg");
-        saiyanPic.src = "img/img" + (i + 1) + ".jpg";
-        saiyanPic.id = (i + 1);
-        saiyanPic.addEventListener("click", function() {
-            addHeart(this.id);
+
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "picture-holder";
+        pictureHolder.id = "picture-holder-" + i;
+        pics.appendChild(pictureHolder);
+    }
+}
+
+function createSaiyanPics() {
+    pictureHolders = document.getElementsByClassName("picture-holder");
+    for (var i = 0; i < pictureHolders.length; i++){
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet_" + (i + 1);
+        saiyanPicture = document.createElement("img");
+        saiyanPicture.src = "img/img" + (i + 1) + ".jpg";
+        saiyanPicture.id = (i + 1);
+        saiyanPicture.addEventListener("click", function() {
+            makeFav(this.id);
         });
-        favorite = document.getElementsByClassName("extra");
-        favorite.id = "extra_" + (i + 1);
+        pictureHolders[i].appendChild(favoriet);
+        pictureHolders[i].appendChild(saiyanPicture);
+
     }
 }
+function makeFav(id ) {
+    notfav = document.getElementsByClassName("favoriet");
 
-function addHeart(id) {
-    clearImages();
-    favorite = document.getElementById("extra_" + id);
-    favorite.src = "img/heart.png";
-}
-
-// img1.addEventListener("click", addImage1);
-// img2.addEventListener("click", addImage2);
-// img3.addEventListener("click", addImage3);
-// img4.addEventListener("click", addImage4);
-// img5.addEventListener("click", addImage5);
-// img6.addEventListener("click", addImage6);
-// img7.addEventListener("click", addImage7);
-// img8.addEventListener("click", addImage8);
-// img9.addEventListener("click", addImage9);
-//
-function clearImages() {
-    for (var i = 1; i < 10; i++) {
-        document.getElementById("extra" + i).src = "";
+    for (var i = 0; i < notfav.length; i++){
+        notfav[i].style.backgroundImage = "none";
     }
-}
-//
-// function addImage1() {
-//     clearImages();
-//     document.getElementById("extra1").src = "img/heart.png";
-// }
-//
-// function addImage2() {
-//     clearImages();
-//     document.getElementById("extra2").src = "img/heart.png";
-// }
-//
-// function addImage3() {
-//     clearImages();
-//     document.getElementById("extra3").src = "img/heart.png"
-// }
-//
-// function addImage4() {
-//     clearImages();
-//     document.getElementById("extra4").src = "img/heart.png";
-// }
-//
-// function addImage5() {
-//     clearImages();
-//     document.getElementById("extra5").src = "img/heart.png";
-// }
-//
-// function addImage6() {
-//     clearImages();
-//     document.getElementById("extra6").src = "img/heart.png";
-// }
-//
-// function addImage7() {
-//     clearImages();
-//     document.getElementById("extra7").src = "img/heart.png";
-// }
-//
-// function addImage8() {
-//     clearImages();
-//     document.getElementById("extra8").src = "img/heart.png";
-// }
-//
-// function addImage9() {
-//     clearImages();
-//     document.getElementById("extra9").src = "img/heart.png";
-// }
 
+    favoriet = document.getElementById("favoriet_" + id);
+    favoriet.style.backgroundImage = "url('img/heart.png')";
+}
 
